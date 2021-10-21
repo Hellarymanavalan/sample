69f01c1c3525145443b47bcbbb25c6265836eaf2
hellarymanavalan <hellarymanavalan939@gmail.com>
2021-10-21 13:44:53 +0530

    manavalan's updated code

--- src/test/java/org/sample/ExcelDataprovider.java
+++ src/test/java/org/sample/ExcelDataprovider.java
@@ -2,13 +2,21 @@ package org.sample;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 public class ExcelDataprovider {
 	
-	private void Excel() throws FileNotFoundException {
+	private void Excel() throws IOException {
 	File f=new File("C:\\Users\\hp\\eclipse-workspace\\maven\\src\\test\\resources\\excel\\Booking details.xlsx");
 	
+	FileInputStream ft=new FileInputStream("C:\\Users\\hp\\Desktop\\My project\\sample\\src\\test\\resources\\excel\\Booking details.xlsx");
+	Workbook w=new XSSFWorkbook(ft);
+	Sheet s=w.getSheet("Sheet1");
 	}
 
 }
